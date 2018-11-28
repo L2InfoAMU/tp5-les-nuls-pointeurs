f2bed6ff5065d5c6add9aa931b25aaa7eeb6d385
luuvuregis-friotemile <regis.luu-vu@etu.univ-amu.fr>
2018-11-28 10:20:37 +0100

    constructors in bruterasterimage

---
+++ src/main/java/image/BruteRasterImage.java
@@ -0,0 +1,34 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class BruteRasterImage implements Image{
+    Color[][] colors;
+    int width;
+    int heigth;
+
+    public BruteRasterImage(Color[][] colors, int width, int heigth) {
+        this.colors = colors;
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    public BruteRasterImage(Color[][] colors) {
+        this.colors = colors;
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+}
